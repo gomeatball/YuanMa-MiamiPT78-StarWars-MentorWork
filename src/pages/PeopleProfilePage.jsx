978f18a342280fdf483b1b6683abf72defee6689
Yuan Ma <mayuan131522@gmail.com>
2025-06-02 03:31:02 +0000

    main

--- src/pages/PeopleProfilePage.jsx
+++ src/pages/PeopleProfilePage.jsx
@@ -1,8 +1,32 @@
+import { useParams } from "react-router-dom"
+import { useEffect, useState } from "react"
+
+
+
+
 export const PeopleProfilePage = () => {
+    const { uid } = useParams();
+    const [character, setCharacter] = useState(null)
+
+    useEffect(()=>{
+          const fetchCharacter = async() => {
+            try { 
+                const response = await fetch(`https://www.swapi.tech/api/people/${uid}`);
+                const data = await response.json();
+                setCharacter(data.result.properties)
+            } catch (error) {
+                console.error("error fetching character", error)
+            }
+          };
+          fetchCharacter();
+          
+    },[uid])
+    if (!character) return <p>Loading...</p>
+
     return (
 
         <>
-        profile page 
+       {character.hair_color}
         </>
     )
 }
